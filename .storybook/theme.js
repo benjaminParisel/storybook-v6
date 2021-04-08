58d07625305f8b73409d539c3a8f3ac3ad72a21e
benjaminParisel <benjamin.parise@gmail.com>
2021-04-08 17:27:16 +0200

    Update to use a custom theme for PA

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'light',
+    brandTitle: 'Process Analytics',
+    brandUrl: 'https://github.com/process-analytics/bpmn-visualization-js',
+    brandImage: 'https://raw.githubusercontent.com/process-analytics/bpmn-visualization-examples/master/demo/static/img/logo_64x64.ico',
+});
